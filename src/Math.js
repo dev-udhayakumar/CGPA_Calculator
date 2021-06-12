7caed58c9eb0f1a336059349f4d2460b16ce202e
udhay5021 <60465112+udhayasiva@users.noreply.github.com>
2021-06-12 20:42:38 +0530

    stable-1

--- src/Math.js
+++ src/Math.js
@@ -1,37 +1,36 @@
-function Sem1math(){
-    var sub1C =document.getElementById("ceinputid11").value;
-    var sub1 = document.getElementById("gdinputid11").value * sub1C;
-
-    var sub2C =document.getElementById("ceinputid12").value;
-    var sub2 = document.getElementById("gdinputid12").value * sub2C;
-    
-    var sub3C =document.getElementById("ceinputid13").value;
-    var sub3 = document.getElementById("gdinputid13").value * sub3C;
-    
-    var sub4C =document.getElementById("ceinputid14").value;
-    var sub4 = document.getElementById("gdinputid14").value * sub4C;
-
-    var sub5C =document.getElementById("ceinputid15").value;
-    var sub5 = document.getElementById("gdinputid15").value * sub5C;
-
-    var sub6C =document.getElementById("ceinputid16").value;
-    var sub6 = document.getElementById("gdinputid16").value * sub6C;
-    
-    var sub7C =document.getElementById("ceinputid17").value;
-    var sub7 = document.getElementById("gdinputid17").value * sub7C;
-    
-    var sub8C =document.getElementById("ceinputid18").value;
-    var sub8 = document.getElementById("gdinputid18").value * sub8C;
-
-    var sub9C =document.getElementById("ceinputid19").value;
-    var sub9 = document.getElementById("gdinputid19").value * sub9C;
-    
-    var sub10C =document.getElementById("ceinputid110").value;
-    var sub10 = document.getElementById("gdinputid110").value * sub10C;
-    
-
-    var TotalCe = sub1C + sub2C + sub3C + sub4C + sub5C + sub6C + sub7C + sub8C + sub9C + sub10C ;
-    var GPA = ( sub1 + sub2 + sub3 + sub4 + sub5 + sub6 + sub7 + sub8 + sub9 + sub10  ) / TotalCe;
-    console.log(GPA);
-    
+var scgpa =[0,0,0,0,0,0,0,0]
+var credit =[0,0,0,0,0,0,0,0]
+function sum(l,m){
+    gp =parseInt(0)
+    cred = parseInt(0)
+    for (i = 1; i <= m; i++){
+        ge = document.getElementById("gdinputid"+ l + i).value
+        ce =  parseInt(document.getElementById("ceinputid" +l + i).value)
+        ans = parseInt(ge * ce)
+        gp = parseInt(gp + ans);
+        cred = parseInt(cred  + ce)
+    }
+    sgpa = gp / cred;
+    fsgpa = sgpa.toFixed(2);
+    document.getElementById("p1" + l).innerHTML = "SGPA : " + fsgpa + " Total credit : " +cred;
+    z = l - 1
+    scgpa[z] = gp;
+    credit[z] = cred
+    console.log("semester "+ l +" scgpa : " + sgpa )
+    console.log("semester " +  l + " total credit : " + cred )
+    console.log("----------------------------")
+    console.log(scgpa)
+    console.log(credit)
+    console.log("----------------------------")
+    cgpa = 0;
+    totcre = 0;
+    for (i=0 ; i<8 ; i++){
+        cgpa = ( cgpa + scgpa[i])
+    }
+    for (i=0 ; i<8 ; i++){
+        totcre = parseInt( totcre + credit[i])
+    }
+    totcgpa = cgpa / totcre
+    last = totcgpa.toFixed(2);
+    document.getElementById("totcgpa").innerHTML = last +" / 10";
 }
--- src/Semester.js
+++ src/Semester.js
@@ -3,9 +3,7 @@ document.getElementById("rolltag").innerHTML = "RollNo : " + localStorage['keyro
 document.getElementById("deptag").innerHTML = "Department : " + localStorage['keydept'];
 
 var a = 1;
-
 AddNewSem(); 
- 
 function AddNewSem(){
     var li = document.createElement("li");
     var id = "id"+ a;
@@ -34,9 +32,9 @@ function AddNewSem(){
           var p1id = "p1" + a;
           var p1 = document.createElement("p");
           p1.setAttribute("id",p1id);
-          p1.setAttribute("class" ,"fs-6");
+          p1.setAttribute("class" ,"fs-6 text-dark");
           document.getElementById(div1id).appendChild(p1);
-          document.getElementById(p1id).innerHTML ="GPA : " + 0 + " Total credit : " + 0;
+          document.getElementById(p1id).innerHTML ="SGPA : " + 0 + " Total credit : " + 0;
         }
       }
       function Editbtn(){
@@ -77,7 +75,7 @@ function AddNewSem(){
         function content(){
           modelTitle();
           modelbody();
-          modelFoot();
+          
       
           function modelTitle(){
             var divHead = document.createElement("div");
@@ -93,22 +91,17 @@ function AddNewSem(){
             document.getElementById(divHeadid).appendChild(Title);
             document.getElementById(Titleid).innerHTML = "Semester " + a ;
     
-            var Addsub = document.createElement("button");
-            var Addsubid = "Addsubid" + a;
-            Addsub.setAttribute("id",Addsubid);
-            Addsub.setAttribute("class","btn btn-sm btn-primary col-4");
-            Addsub.setAttribute("onclick","tr(" + a +",1)");
-            document.getElementById(divHeadid).appendChild(Addsub);
-            document.getElementById(Addsubid).innerHTML = "Add Sub" ;
+            
+            var btnsubmit = document.createElement("button");
+            var btnsubmitid = "Submit" + a;
+            btnsubmit.setAttribute("id",btnsubmitid);
+            btnsubmit.setAttribute("data-bs-dismiss","modal");
+            btnsubmit.setAttribute("onclick","sum()");
+            btnsubmit.setAttribute("class","btn btn-primary ");
+            document.getElementById(divHeadid).appendChild(btnsubmit);
+            document.getElementById(btnsubmitid).innerHTML ="Submit";
     
-            var Delsub = document.createElement("button");
-            var Delsubid = "Delsubid" + a;
-            Delsub.setAttribute("id",Delsubid);
-            Delsub.setAttribute("onclick","delSub(" + a +",0)");
-            Delsub.setAttribute("class","btn btn-sm btn-outline-danger col-2");
-            document.getElementById(divHeadid).appendChild(Delsub);
-            document.getElementById(Delsubid).innerHTML = "Del" ;
-    
+            
             
           }
           function modelbody(){
@@ -117,10 +110,45 @@ function AddNewSem(){
             divBody.setAttribute("id",divBodyid);
             divBody.setAttribute("class","modal-body");
             document.getElementById(div3id).appendChild(divBody);
+
+            var divbtn =document.createElement("div")
+            var divbtnid = "divbtn"+a;
+            divbtn.setAttribute("id",divbtnid);
+            divbtn.setAttribute("class","row");
+            document.getElementById(divBodyid).appendChild(divbtn)
+
+            var divbtnadd =document.createElement("div")
+            var divbtnaddid = "divbtnadd"+a;
+            divbtnadd.setAttribute("id",divbtnaddid);
+            divbtnadd.setAttribute("class","p-3 col-8");
+            document.getElementById(divbtnid).appendChild(divbtnadd)
+
+            var divbtndel =document.createElement("div")
+            var divbtndelid = "divbtndel"+a;
+            divbtndel.setAttribute("id",divbtndelid);
+            divbtndel.setAttribute("class","p-3 col-4");
+            document.getElementById(divbtnid).appendChild(divbtndel)
+
+            var Addsub = document.createElement("button");
+            var Addsubid = "Addsubid" + a;
+            Addsub.setAttribute("id",Addsubid);
+            Addsub.setAttribute("class","btn btn-sm btn-success col-md-12");
+            Addsub.setAttribute("onclick","tr(" + a +",1)");
+            document.getElementById(divbtnaddid).appendChild(Addsub);
+            document.getElementById(Addsubid).innerHTML = "Add Subject" ;
+            
+            var Delsub = document.createElement("button");
+            var Delsubid = "Delsubid" + a;
+            Delsub.setAttribute("id",Delsubid);
+            Delsub.setAttribute("onclick","delSub(" + a +",0)");
+            Delsub.setAttribute("class","btn btn-sm btn-danger col-md-12 ");
+            document.getElementById(divbtndelid).appendChild(Delsub);
+            document.getElementById(Delsubid).innerHTML = "Delete Subject" ;
     
+
             
+
             table()
-            
             function table(){
               
               var table = document.createElement("table");
@@ -130,6 +158,7 @@ function AddNewSem(){
               document.getElementById(divBodyid).appendChild(table);
               tableHead();
               tableBody();
+              
             
               function tableHead(){
                 var thead = document.createElement("thead");
@@ -181,8 +210,10 @@ function AddNewSem(){
                 tbody.setAttribute("id",tbodyid);
                 tbody.setAttribute("class","p-3");
                 document.getElementById(tableid).appendChild(tbody);
+                tr(a,1);tr(a,2);tr(a,3);tr(a,4);tr(a,5);tr(a,6);
                 
-                
+
+               
     
               }   
             }
@@ -195,13 +226,7 @@ function AddNewSem(){
             divFoot.setAttribute("class","modal-footer");
             document.getElementById(div3id).appendChild(divFoot);
       
-            var btnFoot = document.createElement("div");
-            var btnFootid = "btnFoot" + a;
-            btnFoot.setAttribute("id",btnFootid);
-            btnFoot.setAttribute("data-bs-dismiss","modal");
-            btnFoot.setAttribute("class","btn btn-outline-secondary");
-            document.getElementById(divFootid).appendChild(btnFoot);
-            document.getElementById(btnFootid).innerHTML ="Done";
+           
           }
         }
       }
@@ -232,5 +257,171 @@ function AddNewSem(){
     document.getElementById("bt").style.display = "block";
   }
   }
+  function tr(a,i){
 
+    var tbodyid = "tbodyid" + a;  
+    var trbody = document.createElement("tr");
+    var trbodyid = "trbodyid" + a + i;
+    trbody.setAttribute("id",trbodyid);
+    document.getElementById(tbodyid).appendChild(trbody);
+  
+    var thnof = document.createElement("th");
+    var thbodyid = "thnoidf" + a + i;
+    thnof.setAttribute("id",thbodyid);
+    thnof.setAttribute("sope","row");
+    document.getElementById(trbodyid).appendChild(thnof);
+    document.getElementById(thbodyid).innerHTML = i;
+    subject();
+    grade();
+    credit();
 
+    function subject(){
+      var tdsub = document.createElement("td");
+      var tdsubid = "tdsubid" + a + i;
+      tdsub.setAttribute("id",tdsubid);
+      document.getElementById(trbodyid).appendChild(tdsub);
+      
+      var subinput = document.createElement("input");
+      var subinputid = "subinputid" + a + i ;
+      subinput.setAttribute("id",subinputid);
+      subinput.setAttribute("type","text");
+      subinput.setAttribute("class","form-control");
+      subinput.setAttribute("placeholder","subject");
+      document.getElementById(tdsubid).appendChild(subinput);
+    }
+
+    function grade(){
+      var tdgd = document.createElement("td");
+      var tdgdid = "tdgdid" + a + i;
+      tdgd.setAttribute("id",tdgdid);
+      document.getElementById(trbodyid).appendChild(tdgd);
+      
+      var gdinput = document.createElement("select");
+      var gdinputid = "gdinputid" + a + i ;
+      gdinput.setAttribute("id",gdinputid);
+      gdinput.setAttribute("class","form-control");
+      gdinput.setAttribute("aria-label","Default select example");
+      document.getElementById(tdgdid).appendChild(gdinput);
+      
+      options()
+      function options(){
+        var gdoption0 = document.createElement("option");
+        gdoption0id = "gdoption0id" + a + i;
+        gdoption0.setAttribute("selected","");
+        gdoption0.setAttribute("value","0");
+        gdoption0.setAttribute("id",gdoption0id);
+        document.getElementById(gdinputid).appendChild(gdoption0);
+        document.getElementById(gdoption0id).innerHTML = "Grade or GP";
+
+        var gdoption1 = document.createElement("option");
+        gdoption1id = "gdoption1id" + a + i;
+        gdoption1.setAttribute("value","10");
+        gdoption1.setAttribute("id",gdoption1id);
+        document.getElementById(gdinputid).appendChild(gdoption1);
+        document.getElementById(gdoption1id).innerHTML = "O";
+
+        var gdoption2 = document.createElement("option");
+        gdoption2id = "gdoption2id" + a + i;
+        gdoption2.setAttribute("value","9");
+        gdoption2.setAttribute("id",gdoption2id);
+        document.getElementById(gdinputid).appendChild(gdoption2);
+        document.getElementById(gdoption2id).innerHTML = "A+";
+
+        var gdoption3 = document.createElement("option");
+        gdoption3id = "gdoption3id" + a + i;
+        gdoption3.setAttribute("value","8");
+        gdoption3.setAttribute("id",gdoption3id);
+        document.getElementById(gdinputid).appendChild(gdoption3);
+        document.getElementById(gdoption3id).innerHTML = "A";
+
+        var gdoption4 = document.createElement("option");
+        gdoption4id = "gdoption4id" + a + i;
+        gdoption4.setAttribute("value","7");
+        gdoption4.setAttribute("id",gdoption4id);
+        document.getElementById(gdinputid).appendChild(gdoption4);
+        document.getElementById(gdoption4id).innerHTML = "B+";
+
+        var gdoption5 = document.createElement("option");
+        gdoption5id = "gdoption5id" + a + i;
+        gdoption5.setAttribute("value","6");
+        gdoption5.setAttribute("id",gdoption5id);
+        document.getElementById(gdinputid).appendChild(gdoption5);
+        document.getElementById(gdoption5id).innerHTML = "B";
+
+        var gdoption6 = document.createElement("option");
+        gdoption6id = "gdoption6id" + a + i;
+        gdoption6.setAttribute("value","0");
+        gdoption6.setAttribute("id",gdoption6id);
+        document.getElementById(gdinputid).appendChild(gdoption6);
+        document.getElementById(gdoption6id).innerHTML = "U";
+      }
+    }
+    function credit(){
+      
+      var tdce = document.createElement("td");
+      var tdceid = "tdceid" + a + i;
+      tdce.setAttribute("id",tdceid);
+      document.getElementById(trbodyid).appendChild(tdce);
+      
+      var ceinput = document.createElement("select");
+      var ceinputid = "ceinputid" + a + i ;
+      ceinput.setAttribute("id",ceinputid);
+      ceinput.setAttribute("class","form-control");
+      ceinput.setAttribute("aria-label","Default select example");
+      document.getElementById(tdceid).appendChild(ceinput);
+      options()
+      function options(){
+        var ceoption0 = document.createElement("option");
+        ceoption0id = "ceoption0id" + a + i;
+        ceoption0.setAttribute("selected","");
+        ceoption0.setAttribute("value","0");
+        ceoption0.setAttribute("id",ceoption0id);
+        document.getElementById(ceinputid).appendChild(ceoption0);
+        document.getElementById(ceoption0id).innerHTML = "Credit";
+    
+        var ceoption1 = document.createElement("option");
+        ceoption1id = "ceoption1id" + a + i;
+        ceoption1.setAttribute("value","1");
+        ceoption1.setAttribute("id",ceoption1id);
+        document.getElementById(ceinputid).appendChild(ceoption1);
+        document.getElementById(ceoption1id).innerHTML = "1";
+    
+        var ceoption2 = document.createElement("option");
+        ceoption2id = "ceoption2id" + a + i;
+        ceoption2.setAttribute("value","2");
+        ceoption2.setAttribute("id",ceoption2id);
+        document.getElementById(ceinputid).appendChild(ceoption2);
+        document.getElementById(ceoption2id).innerHTML = "2";
+    
+        var ceoption3 = document.createElement("option");
+        ceoption3id = "ceoption3id" + a + i;
+        ceoption3.setAttribute("value","3");
+        ceoption3.setAttribute("id",ceoption3id);
+        document.getElementById(ceinputid).appendChild(ceoption3);
+        document.getElementById(ceoption3id).innerHTML = "3";
+    
+        var ceoption4 = document.createElement("option");
+        ceoption4id = "ceoption4id" + a + i;
+        ceoption4.setAttribute("value","4");
+        ceoption4.setAttribute("id",ceoption4id);
+        document.getElementById(ceinputid).appendChild(ceoption4);
+        document.getElementById(ceoption4id).innerHTML = "4"; 
+      }
+      
+    } 
+    j = i +1;
+   document.getElementById("Addsubid"+ a ).setAttribute("onclick","tr("+ a + "," + j + ")");
+   document.getElementById("Delsubid"+ a ).setAttribute("onclick","delSub("+ a + "," + i + ")");
+   document.getElementById("Submit" + a).setAttribute("onclick","sum("+ a + "," + i + ")");
+  } 
+
+function delSub(a,i){
+    var id = "trbodyid" + a + i;
+    var del =  document.getElementById(id);
+    del.remove();
+    j = i - 1;
+    document.getElementById("Addsubid"+ a ).setAttribute("onclick","tr("+ a + "," + i + ")");
+    document.getElementById("Delsubid"+ a ).setAttribute("onclick","delSub("+ a + "," + j + ")");
+    document.getElementById("Submit"+ a ).setAttribute("onclick","sum("+ a + "," + j + ")");
+    
+} 
